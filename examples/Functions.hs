7ccb78e7cff1bb6919587fe17437d0e1a5f83ea5
George Giorgidze <giorgidze@gmail.com>
2010-10-21 17:08:45 +0200

    Functions example;

--- examples/Functions.hs
+++ examples/Functions.hs
@@ -6,34 +6,32 @@ import Hydra
 
 functions :: SR ()
 functions = [$rel| () ->
-    local x x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 x13 x14 x15 x16 x17 x18 x19 x20 x21 x22 x23 x24
-    x = 2 + time ^ 2 / 100
-    x1  = time
-    x2  = x2
-    x3  = x + x - 2 * x
-    x4  = x - x
-    x5  = x / x
-    x6  = x * x / x ^ 2
-    x7  = x ^ 2 / x * x
-    x8  = logBase x x
-    x9  = -     x
-    x10 = exp   x
-    x11 = log   x
-    x12 = sqrt  x
-    x13 = sin   x
-    x14 = cos   x
-    x15 = tan   x
-    x16 = asin  (1 / (1 + x))
-    x17 = acos  (1 / (1 + x))
-    x18 = atan  (1 / (1 + x))
-    x19 = sinh  x
-    x20 = cosh  x
-    x21 = tanh  x
-    x22 = asinh x
-    x23 = acosh (1 + x)
-    x24 = atanh (1 / (1 + x))
-    -- x25 = abs (sin time)
-    -- x26 = sgn (1 + time)
+    local x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 x13 x14 x15 x16 x17 x18 x19 x20 x21 x22 x23 x24 x25
+    x1 = time
+    x2  = time + 1
+    x3  = time - 1
+    x4  = time / 2
+    x5  = time * 2
+    x6  = time ^ 2
+    x7  = logBase (time + 2) (time + 1)
+    x8  = - time
+    x9  = exp time
+    x10 = log (time + 1)
+    x11 = sqrt time
+    x12 = sin time
+    x13 = cos time
+    x14 = tan time
+    x15 = asin (1 / (time + 1))
+    x16 = acos (1 / (time + 1))
+    x17 = atan time
+    x18 = sinh time
+    x19 = cosh time
+    x20 = tanh time
+    x21 = asinh time
+    x22 = acosh (time + 1)
+    x23 = atanh (1 / (time + 2))
+    x24 = abs (sin time)
+    x25 = signum (sin time) 
 |]
 
 main :: IO ()
