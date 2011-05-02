f6ed5e711500b3155168e14f57da4ea6a8004d3e
George Giorgidze <giorgidze@gmail.com>
2011-05-03 00:52:39 +0200

    Removing simplification phase from flattening;

--- src/Hydra/Stages/Flatten.hs
+++ src/Hydra/Stages/Flatten.hs
@@ -13,16 +13,12 @@ flatten symtab = go (symtab{equations=[],variables = Map.empty}) (flattenEqs 0 (
   where
   go :: SymTab -> [Equation] -> SymTab
   go acc []                         = acc
-  go acc ((Equal e1 e2) : eqs)      = let e3 = simplify e1
-                                          e4 = simplify e2
-                                          acc1 = buildVars (buildVars acc e3) e4
-                                      in  go (acc1 {equations = (PrimApp Sub (Pair e3 e4)) : (equations acc1)}) eqs
-  go acc ((Init  (Var i) e1) : eqs) = let e2 = simplify e1
-                                          acc1 = buildVars acc e2
-                                      in  go (acc1 {variables = Map.insert i (Just (eval acc1 e2)) (variables acc1)}) eqs
-  go acc ((Init (PrimApp Mul (Pair (Const (-1)) e1)) e2) : eqs) = go acc ((Init e1 (PrimApp Neg (simplify e2))) : eqs)
+  go acc ((Equal e1 e2) : eqs)      = let acc1 = buildVars (buildVars acc e1) e2
+                                      in  go (acc1 {equations = (PrimApp Sub (Pair e1 e2)) : (equations acc1)}) eqs
+  go acc ((Init  (Var i) e1) : eqs) = let acc1 = buildVars acc e1
+                                      in  go (acc1 {variables = Map.insert i (Just (eval acc1 e1)) (variables acc1)}) eqs
   go _   ((Init _ _) : _)           = error "This version of Hydra only supports directed (i.e., caulal) init and reinit equations."
-  go _   _                         = $impossible
+  go _   _                          = $impossible
 
 flattenEqs :: Int -> [Equation] -> [Equation]
 flattenEqs _ []         = []
@@ -50,16 +46,3 @@ buildVars acc e = case e of
 
   PrimApp _ e1 -> buildVars acc e1
   Pair e1 e2 -> buildVars (buildVars acc e1) e2
-
-simplify :: Signal a -> Signal a
-simplify e = case e of
-  Unit    -> e
-  Time    -> e
-  Const _ -> e
-  Var   _ -> e
-  PrimApp Der (Var _) -> e
-  PrimApp Der (PrimApp Mul (Pair (Const (-1)) e1)) -> PrimApp Neg (simplify (PrimApp Der e1))
-  PrimApp Der _ -> error "This version of Hydra only supports first order derivatives."
-  PrimApp f1 e1 -> PrimApp f1 (simplify e1)
-  Pair e1 e2 -> Pair (simplify e1) (simplify e2)
-
